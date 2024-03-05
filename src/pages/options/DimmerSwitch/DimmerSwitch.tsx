7886709a1d873ac26afad862e751ac134e7ed1a7
Paweł Wiewióra <kaczka097@gmail.com>
2024-03-05 02:12:04 +0100

    feat: hook DimmerSwitch

--- src/pages/options/DimmerSwitch/DimmerSwitch.tsx
+++ src/pages/options/DimmerSwitch/DimmerSwitch.tsx
@@ -1,6 +1,10 @@
 import { FormControl, FormLabel, Switch } from '@chakra-ui/react';
+import useStorage from '@root/src/shared/hooks/useStorage';
+import dimmerStateStorage from '@root/src/shared/storages/DimmerStateStorage';
 
 const DimmerSwitch = () => {
+   const isDimmerEnabled = useStorage(dimmerStateStorage);
+
    return (
       <FormControl
          display="flex"
@@ -12,7 +16,11 @@ const DimmerSwitch = () => {
          >
             Enable screen dimmer
          </FormLabel>
-         <Switch id="dimmer-switch" />
+         <Switch
+            id="dimmer-switch"
+            isChecked={isDimmerEnabled}
+            onChange={dimmerStateStorage.toggle}
+         />
       </FormControl>
    );
 };
